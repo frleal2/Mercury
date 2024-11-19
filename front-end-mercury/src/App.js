40d585414b06dd6fcc895c5af52a4b157cb733eb
frleal2 <felipe.leal.sierra@gmail.com>
2024-11-18 22:54:17 -0700

    Beginning of project setup.

--- front-end-mercury/src/App.js
+++ front-end-mercury/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
+import Header from './components/Header';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path ='/' elemnt = {<Home />} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 }
 
---
+++ front-end-mercury/src/Pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <div>
+            <h1> LANDING PAGE</h1>
+        </div>
+
+    );
+}
