3aa6e4e38fe0a8f1d0992dc3186f7a6dccd99f84
frleal2 <felipe.leal.sierra@gmail.com>
2025-04-17 13:11:58 -0600

    added django settings

--- back-end/build.sh
+++ back-end/build.sh
@@ -5,8 +5,11 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Export the correct settings module for Render deployment
+export DJANGO_SETTINGS_MODULE=MercApi.deployment_settings
+
 # Convert static asset files
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input || echo "collectstatic failed, continuing..."
 
 # Apply any outstanding database migrations
 python manage.py migrate
