64c018f3291aa11e826b932d15be422b2328dd52
frleal2 <felipe.leal.sierra@gmail.com>
2025-04-17 14:43:01 -0600

    changed settings.

--- back-end/MercAPI/settings.py
+++ back-end/MercAPI/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'MercApi.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173'
+    'http://localhost:5173',
     'http://localhost:3000',
 ]
 
--- back-end/manage.py
+++ back-end/manage.py
@@ -5,9 +5,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MercApi.settings')  # Ensure this is set
     settings_module = 'MercApi.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'MercApi.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
