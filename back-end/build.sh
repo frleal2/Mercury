bb54e0c42b2128c5e24a5c3a11e54093c297eb43
frleal2 <felipe.leal.sierra@gmail.com>
2025-04-17 12:00:55 -0600

    edit build

--- back-end/build.sh
+++ back-end/build.sh
@@ -1,8 +1,10 @@
-set -0 errexit
+set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+gunicorn MercApi.wsgi:application --bind 0.0.0.0:$PORT  # Corrected gunicorn command
 
