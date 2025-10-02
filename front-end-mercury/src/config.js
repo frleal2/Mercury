023596f0efeeb2024c0903c240887ee154078f35
frleal2 <felipe.leal.sierra@gmail.com>
2025-10-02 14:55:37 -0600

    configuring to know what env it is using.

--- front-end-mercury/src/config.js
+++ front-end-mercury/src/config.js
@@ -1,5 +1,10 @@
-const BASE_URL = process.env.REACT_APP_ENV === 'render'
-  ? process.env.REACT_APP_RENDER_API_URL.replace(/\/$/, '') // Remove trailing slash
-  : process.env.REACT_APP_LOCAL_API_URL.replace(/\/$/, ''); // Remove trailing slash
+const ENV = process.env.REACT_APP_ENV || 'render'; // Default to 'render' if not specified
+
+const API_URLS = {
+  local: process.env.REACT_APP_LOCAL_API_URL,
+  render: process.env.REACT_APP_RENDER_API_URL,
+};
+
+const BASE_URL = API_URLS[ENV] || process.env.REACT_APP_API_URL; // Fallback to REACT_APP_API_URL
 
 export default BASE_URL;
