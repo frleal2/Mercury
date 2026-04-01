136b9cb081285f16c3900deeb2eb5482109b1b45
frleal2 <felipe.leal.sierra@gmail.com>
2026-04-01 16:31:40 -0600

    fix: use deployment_settings in Celery worker so FRONTEND_URL resolves to production URL

--- back-end/MercAPI/celery.py
+++ back-end/MercAPI/celery.py
@@ -1,8 +1,9 @@
 import os
 from celery import Celery
 
-# Set default Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MercAPI.settings')
+# Set default Django settings module (use deployment_settings on Render)
+settings_module = 'MercAPI.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'MercAPI.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 app = Celery('MercAPI')
 
